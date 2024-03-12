a62e0ad7d9b8dce92e1dc06d072ff007e8d587b9
Aileen Sun <aileensun626@gmail.com>
2024-03-11 18:42:34 -0700

    reorganized packages

--- Packages/tests/test_TrackONautsStats.py
+++ Packages/tests/test_TrackONautsStats.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 
-import TrackONautsStats
+from Packages import TrackONautsStats
 
 df_exp1 = pd.DataFrame([[-1,0,1],[1,0,-1],[0.5,0,0.5]], index=["A","B","C"])
 df_exp2 = pd.DataFrame([[1,0,1],[-1,0,-1],[0,0,0]], index=["A","B","C"])
