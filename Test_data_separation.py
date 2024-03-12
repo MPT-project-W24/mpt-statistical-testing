b998fbb028353a431e1483d0355a9639be63aa83
mengqinqqq <qinmengviola@gmail.com>
2024-03-11 20:30:14 -0700

    change

--- Test_data_separation.py
+++ Test_data_separation.py
@@ -1,13 +1,54 @@
 import unittest
 from unittest import TestCase
-from data_separation import read_feature, filter_feature, remive_nans_feature, read_json,  put_together
+from Packages.data_separation import read_feature, filter_feature, remove_nans_feature, read_json,  put_together
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from os import listdir, getcwd, chdir
 from os.path import isfile, join
 import json
 import os
+
+feature_list = [
+    'alpha', # Fitted anomalous diffusion alpha exponenet
+    'D_fit', # Fitted anomalous diffusion coefficient
+    'kurtosis', # Kurtosis of track
+    'asymmetry1', # Asymmetry of trajecory (0 for circular symmetric, 1 for linear)
+    'asymmetry2', # Ratio of the smaller to larger principal radius of gyration
+    'asymmetry3', # An asymmetric feature that accnts for non-cylindrically symmetric pt distributions
+    'AR', # Aspect ratio of long and short side of trajectory's minimum bounding rectangle
+    'elongation', # Est. of amount of extension of trajectory from centroid
+    'boundedness', # How much a particle with Deff is restricted by a circular confinement of radius r
+    'fractal_dim', # Measure of how complicated a self similar figure is
+    'trappedness', # Probability that a particle with Deff is trapped in a region
+    'efficiency', # Ratio of squared net displacement to the sum of squared step lengths
+    'straightness', # Ratio of net displacement to the sum of squared step lengths
+    'MSD_ratio', # MSD ratio of the track
+#     'frames', # Number of frames the track spans
+    'Deff1', # Effective diffusion coefficient at 0.33 s
+    'Deff2', # Effective diffusion coefficient at 3.3 s
+    'Mean alpha', 
+    'Mean D_fit', 
+    'Mean kurtosis', 
+    'Mean asymmetry1', 
+    'Mean asymmetry2',
+    'Mean asymmetry3', 
+    'Mean AR',
+    'Mean elongation', 
+    'Mean boundedness',
+    'Mean fractal_dim', 
+    'Mean trappedness', 
+    'Mean efficiency',
+    'Mean straightness', 
+    'Mean MSD_ratio', 
+    'Mean Deff1', 
+    'Mean Deff2',
+    ]
+feature_data_path = '/Users/xihongshijidanzhajiangmian/Desktop/uw/courses/winter2024/software/feature_data/'
+feature_files = [f for f in listdir(feature_data_path) if isfile(join(feature_data_path, f)) and '.csv' in f and 'P' in f]
+json_filepath = '/Users/xihongshijidanzhajiangmian/Desktop/uw/courses/winter2024/software/15_models_10_percent.json'
+f1 = open(json_filepath, 'r')
+json_data = json.load(f1)
+f1.close()
 
 
 class TestSeparation(unittest.TestCase):
